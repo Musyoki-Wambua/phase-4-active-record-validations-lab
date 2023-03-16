c1d2ccb5e7c674e2d4cf4441282b84d67f961274
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-03-16 14:09:12 +0300

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum:  250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :clickbait_y
+
+    def clickbait_y
+        unless title&.include?("Won't Believe") || title&.include?("Secret") || title&.match?(/Top \d+/) || title&.include?("Guess")
+          errors.add(:title, "must be clickbait-y")
+        end
+      end
 end
